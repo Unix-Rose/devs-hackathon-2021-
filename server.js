754c02bc3ef9ce10a16655f16b43a0efa02fcbc5
Matthew Jakeman <mjak923@aucklanduni.ac.nz>
2021-07-03 20:50:31 +1200

    Implement Search

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ const CodeSnippet = require('./schema/code_snippet');
 const snipRouter = require('./routes/snip_router');
 const methodOverride = require('method-override');
 
+// lunr search
+const lunr = require('lunr');
+
 mongoose.connect('mongodb://localhost/snippets', { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true});
 
 const app = express();
@@ -17,17 +20,53 @@ app.use(express.static(__dirname + '/public'))
 app.set('view engine', 'ejs');
 
 app.get('/', async (req, res) => {
-    // var snip = new CommandSnippet();
-    // snip.title = "Unzip a gzip tar file";
-    // snip.snippet = "tar -xzvf file_name.tar";
-    // snip.description = "Extract a gzipped tar file archive";
-    // snip.tags = ["tar", "gzip", "extract"];
-    // snip.program = "tar";
-
-    const snippets = await Snippet.find().sort({ meta_date: 'desc' });
+    
+    // TODO: Fill the search bar with current query
+    let snippets = await Snippet.find().sort({ meta_date: 'desc' });
+    snippets = await search(req.query.q);
 
     res.render('index', { snippets: snippets });
 });
+
+async function search(searchQuery) {
+    
+    // Get all snippets
+    const snippets = await Snippet.find();
+    let results = snippets;
+
+    console.log(searchQuery);
+    if (searchQuery != null && searchQuery != undefined) {
+        // Perform full text search
+        var idx = lunr(function () {
+            this.field('title');
+            this.field('snippet');
+            this.field('tags');
+            this.field('category');
+            this.field('program');
+            this.field('language');
+
+            this.ref('_index');
+        
+            let i = 0;
+            snippets.forEach(s => {
+                s._index = i;
+                this.add(s);
+
+                i++;
+            });
+        });
+
+        const searchRefs = idx.search(searchQuery);
+
+        // TODO: Highlighting
+        results = new Array();
+        searchRefs.forEach(async res => {
+            results.push(snippets[res.ref]);
+        });
+    }
+
+    return results;
+}
 
 app.use('/snip', snipRouter);
 
